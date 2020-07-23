e3295bc8f27beeb463098b406f7b145991a9a47f
Ivan <ivan1501@gmail.com>
2020-07-23 13:35:33 +0200

    Top rated movies will show. Load button

--- movies/movies.js
+++ movies/movies.js
@@ -1,3 +1,15 @@
 var urlBase = 'https://api.themoviedb.org/3/';
 var key = '';
 var pageNumber = 1;
+var moviesDiv = document.getElementById('moviesContainer');
+
+const fetchMovie = () =>  {
+    var url = `${urlBase}discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&page=${pageNumber}`; 
+      fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+      getMovies(data)
+    })
+}
+
+fetchMovie();
